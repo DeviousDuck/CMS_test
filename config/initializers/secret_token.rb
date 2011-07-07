91ea072514f20575639efe6829a9b7e6e0ab350f
Matthew Sumner <matthew@garibaysmt.com>
2011-07-07 11:02:03 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMSTest::Application.config.secret_token = '8813dba68f8d92f953354b6cd7b9fdfdfa99a5707e5f28e0846bab2e3d0a7314d09eb73cfc47ba6eb2fa246efaf001bf44991e273a9c0567035b8d289287f033'
